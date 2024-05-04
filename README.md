96a4981319df9cfacb9c7194138647968317dfe8
Bilal Ahmed <bilalahmed815@gmail.com>
2024-05-04 22:24:49 +0500

    Initial push to deploy

---
+++ README.md
@@ -0,0 +1,69 @@
+**Stand Bog** is a **Pelican** **responsive**, **clean-coded**, and **optimized mobile devices** Pelican blog theme developed by [**PyPiThemes**](https://pypithemes.com). It works with **GitHub Pages**, **Cloudflare Pages**, and **Netlify Pages** so that you can host it for **free**.
+
+
+![Stand Blog Home](Home-Stand-Blog-Home.png)
+
+UX that needs to be followed: https://www.behance.net/gallery/194989379/Tech-Thrifty-Blog-Website?tracking_source=search_projects|tech+blog&l=39
+
+Template replicated: https://templatemo.com/tm-551-stand-blog
+
+## Future Additions (Source: https://www.github-zh.com/projects/6547965-pelican-plugins):
+https://github.com/nogaems/pelican-ert
+./post_stats
+https://github.com/pelican-plugins/read-more
+https://github.com/pelican-plugins/related-posts
+https://github.com/pelican-plugins/photos
+https://github.com/kura/pelican_youtube
+https://github.com/jhshi/pelican.plugins.ga_page_view
+https://github.com/kura/pelican-githubprojects
+https://github.com/ingwinlu/pelican-toc
+https://github.com/Shaked/pelican-version
+https://github.com/tylerdave/pelican-meetup-info
+https://github.com/mortada/pelican_javascript
+https://github.com/bmcorser/pelicanfly
+https://pypi.org/project/pelican_admin/#toc-entry-1
+
+
+Demo:
+-----
+
+Experience the theme in action by visiting our live demo: [Demo](https://stand-blog.pages.dev/)
+
+Features:
+---------
+
+*   **Section featured posts:** Showcase your best posts in a dedicated section.
+*   **Section Blog:** Display your latest blog posts in a stylish and easy-to-read way.
+*   **Section videos:** Share your videos with your readers in a dedicated section.
+*   **Section author:** Give your authors their own page to showcase their work.
+*   **Section tag:** Organize your posts by tags for easy navigation.
+*   **Loading page:** Display a loading animation while your page loads.
+*   **Super fast performance:** Stand Bog is optimized for speed and performance so your readers can enjoy your blog without lag.
+*   **Social sharing buttons:** Let your readers easily share your posts on social media.
+*   **Scroll to the top button:** Easily navigate back to the top of your page with a single click.
+*   **Modern search form:** Find the posts you want with a modern and intuitive search form.
+*   **Compatible with modern browsers:** Stand Bog is compatible with all modern browsers, so your readers can enjoy your blog regardless of their device.
+*   **Support Meta tags and OpenGraph:** Stand Bog supports Meta tags and OpenGraph and is easy to customize.
+*   **Image lazy loading:** Only load images when visible on the screen, improving performance.
+*   **Image Gallery:** Create beautiful image galleries with ease.
+*   **Author page:** Each author has their own page to showcase their work.
+*   **Custom Icon support:** Upload your own icon to give your blog a unique look.
+*   **Supports video posts:** Easily add videos to your posts.
+*   **Supports multiple authors:** Easily manage multiple authors with Stand Bog.
+*   **Supports contact form (Formspree):** Let your readers easily contact you using a Formspree contact form.
+*   **Supports Disqus comments:** Enable Disqus comments on your blog so your readers can discuss your posts.
+*   **Supports Google Analytics:** Track your website traffic with Google Analytics.
+*   **Ionicons icons:** Use beautiful Ionicons icons throughout your blog.
+*   **Free Google Fonts:** Style your blog with over 1,000 free Google Fonts.
+*   **Free updates & support:** Stand Bog is constantly updated with new features and bug fixes. You also get free support from the PyPiThemes team.
+
+## To debug:
+pelican content --debug > pelicandebug.txt
+
+Technologies:
+-------------
+
+*   Pelican==4.9.1
+*   Pelican-share-post==1.0.0
+*   Bootstrap==v4.1.3
+*   Font Awesome==4.3.0
---
+++ pelicanconf.py
@@ -0,0 +1,155 @@
+# Import required module
+import os
+
+# Author and site information
+AUTHOR = 'Bilal Khalid'
+SITENAME = 'MarketingProInsider'
+SITEURL = ''
+CATEGORY = 'Blog'
+SITEDESCRIPTION = 'My Blog'
+DEFAULT_LOCALE = 'en_US'
+
+# To read markdown file in 
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html',
+}
+
+# Explicitly define the Markdown reader
+from pelican.readers import MarkdownReader
+READERS = {'md': MarkdownReader}
+
+# Contact information
+CONTACT_INFORMATION = {
+    'Phone Number': '+92 342-270-0268',
+    'Email Address': 'captivesmeta@gmail.com',
+    'Street Address': 'Shahrah e Sher Shah Suri, North Nazimabad Town, Karachi, Pakistan',
+    'Map Embed URL': 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28941.979462981402!2d67.0418153!3d24.9406726!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f8014fe5943%3A0x11df7a65effb1ef6!2sNorth%20Nazimabad%20Town%2C%20Karachi%2C%20Karachi%20City%2C%20Sindh!5e0!3m2!1sen!2s!4v1713872000240!5m2!1sen!2s'
+}
+
+# Content settings
+PATH = 'content/'
+
+# Time and language settings
+TIMEZONE = 'Europe/Rome'
+DEFAULT_LANG = 'en'
+
+# Appearance settings
+SITELOGO = ""
+FAVEICON = '{}/theme/assets/images/icon.png'.format(SITEURL)
+DELETE_OUTPUT_DIRECTORY = True
+THEME = 'theme/'
+
+# Feed generation settings
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Article settings
+ARTICLE_URL = '{slug}'
+ARTICLE_SAVE_AS = '{slug}.html'
+
+# Page settings
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# Index settings
+INDEX_URL = '/'
+INDEX_SAVE_AS = 'index.html'
+
+# Category settings
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORY_PAGINATION_URL = 'category/{slug}/page/{number}/'
+CATEGORY_PAGINATION_SAVE_AS = 'category/{slug}/page/{number}/index.html'
+
+# Tag settings
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAG_PAGINATION_URL = 'tag/{slug}/page/{number}/'
+TAG_PAGINATION_SAVE_AS = 'tag/{slug}/page/{number}/index.html'
+
+# Author settings
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_URL_SAVE_AS = 'author/{slug}/index.html'
+
+# Pages
+TEMPLATE_PAGES = {
+    'blog.html': 'blog.html',
+    'contact.html': 'contact.html',
+    '404.html': '404.html',
+}
+
+# Static files
+STATIC_PATHS = ['extra/robots.txt']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+}
+
+# Blogroll links
+LINKS = (
+    ('Pelican', 'https://getpelican.com/'),
+    ('Python.org', 'https://www.python.org/'),
+    ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+    ('You can modify those links in your config file', '#'),
+)
+
+# Social media links
+SOCIAL_MEDIA_LINKS = (
+    ('Twitter', 'https://twitter.com/your_twitter_username'),
+    ('Facebook', 'https://facebook.com/your_facebook_username'),
+    ('GitHub', 'https://github.com/your_github_username'),
+    ('Youtube', 'https://youtube.com/your_youtube_username'),
+    ('Tiktok', ''),
+    # Add more social media links here
+)
+
+# Pagination settings
+DEFAULT_PAGINATION = False
+'''PAGINATION_PATTERNS = (
+    (1, '{url}', '{save_as}'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)'''
+
+# Plugins
+PLUGINS_PATH = os.path.join(os.getcwd(), "pelican-plugins")
+PLUGIN_PATHS = [PLUGINS_PATH]
+PLUGINS = ["sitemap", "articlejson", "share_post"]
+
+# Sitemap settings
+SITEMAP = {
+    'siteurl': SITEURL,
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'daily',
+        'pages': 'daily',
+        'indexes': 'daily',
+    },
+    'exclude': [],  # ex: ['categories', 'tags']
+}
+
+# SEO settings
+OPEN_GRAPH = True
+TWITTER_TAGS = True
+YOUR_TWITTER_HANDLE = "pytheme"
+REL_CANONICAL = True
+USE_GOOGLE_FONTS = True
+
+# KwesForms settings
+KWESFORMS_ACTION = "https://kwesforms.com/api/foreign/forms/S9CLmleGJB99fnH4f3kw"
+
+# Disqus settings
+DISQUS = True
+
+# HIGHLIGHTER = True
